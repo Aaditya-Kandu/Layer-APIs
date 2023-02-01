041365609befec8e4f6e7e0bb0fccd97203fba3e
Aaditya kandu <aadityakandu@gamil.com>
2023-02-01 21:33:16 +0530

    After add ResponseEntity

--- src/main/java/com/example/layerproject/DeveloperController.java
+++ src/main/java/com/example/layerproject/DeveloperController.java
@@ -1,6 +1,9 @@
 package com.example.layerproject;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,26 +13,43 @@ public class DeveloperController {
     DeveloperService developerService;
 
    @PostMapping("/add_dev")
-   public String addDev(@RequestBody Developer developer){
+   public ResponseEntity addDev(@RequestBody Developer developer){
 
-       return developerService.addDev(developer);
+       String resonse = developerService.addDev(developer);
+
+       return new ResponseEntity<>(resonse, HttpStatus.ACCEPTED);
+
+       //return developerService.addDev(developer);
 
     }
     @GetMapping("/get_dev")
-    public Developer getDev(@RequestParam("q") int id){
+    public ResponseEntity getDev(@RequestParam("q") int id){
 
-       return developerService.getDev(id);
+       Developer response = developerService.getDev(id);
+
+       return new ResponseEntity<>(response, HttpStatus.CREATED);
+
+       //return developerService.getDev(id);
     }
 
     @PutMapping("/update_dev")
-    public String updateDev(@RequestParam("id") int id,@RequestParam("salary") int salary,@RequestParam("role") String role){
+    public ResponseEntity updateDev(@RequestParam("id") int id,@RequestParam("salary") int salary,@RequestParam("role") String role){
 
-       return developerService.updateDev(id,salary,role);
+      String response = developerService.updateDev(id, salary, role);
+
+      return new ResponseEntity<>(response , HttpStatus.ACCEPTED);
+
+       // return developerService.updateDev(id,salary,role);
     }
 
     @DeleteMapping("/delete_dev/{id}")
-    public String deleteDev(@PathVariable("id") int id){
-       return developerService.deleteDev(id);
+    public ResponseEntity deleteDev(@PathVariable("id") int id){
+
+       String response = developerService.deleteDev(id);
+
+       return new ResponseEntity<>(response,HttpStatus.FOUND);
+
+       // return developerService.deleteDev(id);
 
     }
 }
